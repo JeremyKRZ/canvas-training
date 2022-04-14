85428905e76efc7d82ef25f5959d8f6e81dd7de0
JeremyKRZ <jeremy.krzeczowski77@gmail.com>
2022-04-14 16:32:15 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,11 +4,30 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
+//rectangles
 c.fillRect(100, 100, 200, 200)
+c.fillStyle = '#ccc'
 c.fillRect(300, 300, 200, 200)
+c.fillStyle = '#00f'
 c.fillRect(500, 100, 200, 200)
 
+//Lines
+c.beginPath()
+c.moveTo(50, 300)
+c.lineTo(700, 50)
+c.strokeStyle = "#f00"
+c.stroke()
 
+//Cercle
+
+for (var i = 0; i < 1000; i++){
+    var x = Math.random() * window.innerWidth
+    var y = Math.random() * window.innerHeight
+    c.beginPath()
+    c.arc(x, y, 30, 0, Math.PI * 2, false)
+    c.strokeStyle = "#f15477"
+    c.stroke()
+}
 
 window.addEventListener('resize', function () {
     canvas.width = window.innerWidth;
